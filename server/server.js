017fc293fd6e0cdc92a1f862af86e92be530c7dc
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-02-08 16:53:57 +0530

    feat: add Clerk authentication middleware to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import cors from "cors";
 import 'dotenv/config';
+import { clerkMiddleware,requireAuth } from '@clerk/express'
 
 // initializing express app
 const app = express();
@@ -9,9 +10,13 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware());
 
 // basic route
 app.get('/', (req,res)=> res.send('Server is Live'))
+
+// all routes will be protected using require auth
+app.use(requireAuth());
 
 // starting the server
 const PORT = process.env.PORT || 3000;

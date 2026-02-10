ccc13bac3d3882ba724f309a72f465f8a2dbfc0b
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-02-11 00:45:20 +0530

    feat: remove package.json, add OpenAI integration and new AI controller
    - Deleted package.json as it is no longer needed.
    - Updated package-lock.json to include OpenAI dependency.
    - Modified server/package.json to add OpenAI and Clerk dependencies.
    - Created ai.controller.js to implement article generation using OpenAI API.
    - Added logic to handle user plans and usage limits for article generation.

---
+++ server/controllers/ai.controller.js
@@ -0,0 +1,57 @@
+import { OpenAI } from "openai/client.js";
+import sql from "../configs/db.js";
+import { clerkClient } from "@clerk/express";
+
+const AI = new OpenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+})
+
+// controller to generate article
+export const generateArticle = async (req, res, next) => {
+
+    try {
+        const { userId } = req.auth();
+        const { prompt, length } = req.body;
+        // getting plan from request
+        const plan = req.plan;
+        // getting user usage from request
+        const user_usage = req.user_usage;
+
+
+        // if user does not have free plan and user also has hit it's free limit then we should stop user to use feature no longer
+        if (plan !== "premium" && user_usage >= 10) {
+            res.json({ success: false, message: "Limit reached. Upgrade to continue." })
+        }
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+            temperature: 0.7,
+            max_ltokens
+        })
+        const content = response.choices[0].message.content;
+
+        await sql` INSERT INTO creations(user_id,prompt,content,type) VALUES (${userId},${prompt},${content},'article')`
+
+        if (plan !== "premium") {
+            clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    user_usage: user_usage + 1
+                }
+            })
+        };
+
+        // responding with content
+        res.json({ success: true, content })
+
+    } catch (error) {
+        console.log(error.message); 
+        res.json({ success: false, message: error.message });
+    }
+};

67157193a393af23d43a5e00745ee49d60facc25
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-03-03 19:08:00 +0530

    fix(api): correct pdf-parse import in ai controller

--- server/controllers/ai.controller.js
+++ server/controllers/ai.controller.js
@@ -4,7 +4,7 @@ import { clerkClient } from "@clerk/express";
 import axios from "axios";
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import pdf from "pdf-parse/lib/pdf-parse.js";
+import {PDFParse} from "pdf-parse";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -246,7 +246,9 @@ export const resumeReview = async (req, res) => {
         }
 
         const dataBuffer = fs.readFileSync(resume.path);
-        const pdfData = await pdf(dataBuffer);
+        const parser = new PDFParse(dataBuffer);
+        const pdfData = await parser.getText(dataBuffer);
+
 
         const prompt = `Review the following resume and provide constructive feedback on its strengths, weaknesses, and areas for improvement. Resume Content:\n\n${pdfData.text}`;
 

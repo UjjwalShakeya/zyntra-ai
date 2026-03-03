4c7ea22028776861bd2c1870dc57cc1975916f5c
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-03-03 19:12:30 +0530

    feat(api): add routes for background removal, object removal, and resume review

--- server/routes/ai.routes.js
+++ server/routes/ai.routes.js
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
-import { generateArticle, generateBlogTitle, generateImage } from "../controllers/ai.controller.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/ai.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
+import { upload } from "../configs/multer.js";
 
 // making instance of router
 const aiRouter = Router();
@@ -8,6 +9,9 @@ const aiRouter = Router();
 aiRouter.post('/generate-article', auth, generateArticle);
 aiRouter.post('/generate-blog-title', auth, generateBlogTitle);
 aiRouter.post('/generate-image', auth, generateImage);
+aiRouter.post('/remove-image-background', upload.single('image'), auth, removeImageBackground);
+aiRouter.post('/remove-image-object', upload.single('image'), auth, removeImageObject);
+aiRouter.post('/resume-review', auth, upload.single('resume'), resumeReview);
 
 export default aiRouter;
 

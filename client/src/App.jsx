56217baecd24727accc59fd32271e28c1c8d0347
UjjwalShakeya <ujjwalshakeya1@gmail.com>
2026-03-05 03:00:03 +0530

    feat(ui): integrate toast notifications using react-hot-toast

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,32 +15,32 @@ import ReviewResume from "./pages/ReviewResume";
 import Community from "./pages/Community";
 import { useEffect } from "react";
 import { useAuth } from "@clerk/clerk-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { getToken } = useAuth();
 
-  useEffect(() => {
-    getToken().then((Token) => console.log(Token));
-  },[])
-
   return (
-    <Routes>
-      {/* route for static home page */}
-      <Route path="/" element={<Home />} />
+    <div>
+      <Toaster />
+      <Routes>
+        {/* route for static home page */}
+        <Route path="/" element={<Home />} />
 
-      {/* nested routes for ai features*/}
-      <Route path="/ai" element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="write-article" element={<WriteArticle />} />
-        <Route path="blog-titles" element={<BlogTitles />} />
-        <Route path="generate-images" element={<GenerateImages />} />
-        <Route path="remove-background" element={<RemoveBackground />} />
-        <Route path="remove-object" element={<RemoveObject />} />
-        <Route path="review-resume" element={<ReviewResume />} />
-        <Route path="community" element={<Community />} />
-      </Route>
+        {/* nested routes for ai features*/}
+        <Route path="/ai" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="write-article" element={<WriteArticle />} />
+          <Route path="blog-titles" element={<BlogTitles />} />
+          <Route path="generate-images" element={<GenerateImages />} />
+          <Route path="remove-background" element={<RemoveBackground />} />
+          <Route path="remove-object" element={<RemoveObject />} />
+          <Route path="review-resume" element={<ReviewResume />} />
+          <Route path="community" element={<Community />} />
+        </Route>
 
-    </Routes>
+      </Routes>
+    </div>
   )
 }
 
